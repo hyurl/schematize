eed0a9684a4b7ce57440c2445fd6e1839b690db8
Ayon Lee <i@hyurl.com>
2020-04-23 00:36:35 +0800

    refine tests of map

--- test/ensure/Map.js
+++ test/ensure/Map.js
@@ -2,16 +2,57 @@
 const assert = require("assert");
 const { ensure } = require("../..");
 
-describe("ensure: Map", () => {
-    const entries = [["foo", "Hello"], ["bar", "World"]];
-    const map = new Map(entries);
+const entries = [["foo", "Hello"], ["bar", "World"]];
+const map = new Map(entries);
 
-    it("should ensure default value", () => {
+describe("ensure: Map", () => {
+    it("should return as-is for existing properties of Map type", () => {
+        assert.deepStrictEqual(
+            ensure({ foo: map }, { foo: Map }),
+            { foo: map }
+        );
+    });
+
+    it("should return as-is for existing sub-properties of Map type", () => {
+        assert.deepStrictEqual(
+            ensure(
+                { foo: { bar: map } },
+                { foo: { bar: Map } }
+            ),
+            { foo: { bar: map } }
+        );
+    });
+
+    it("should cast existing properties of non-url type to Map", () => {
+        assert.deepStrictEqual(
+            ensure({ foo: entries }, { foo: Map }),
+            { foo: map }
+        );
+    });
+
+    it("should cast existing values in sub-node to Maps", () => {
+        assert.deepStrictEqual(
+            ensure({ foo: { bar: entries } }, { foo: { bar: Map } }),
+            { foo: { bar: map } }
+        );
+    });
+
+    it("should cast all elements in an array to Maps by array schema", () => {
+        assert.deepStrictEqual(
+            ensure({ foo: [entries] }, { foo: [Map] }),
+            { foo: [map] }
+        );
+    });
+
+    it("should use an empty map as the default value for missing properties", () => {
         assert.deepStrictEqual(ensure({}, { foo: Map }), { foo: new Map() });
+    });
+
+    it("should use the given map object as the default value for missing properties", () => {
         assert.deepStrictEqual(ensure({}, { foo: map }), { foo: map });
     });
 
-    it("should ensure default value in sub-nodes", () => {
+    it("should create default values in sub-nodes", () => {
         assert.deepStrictEqual(
             ensure({}, { foo: { bar: Map } }),
             { foo: { bar: new Map() } }
@@ -19,54 +60,6 @@ describe("ensure: Map", () => {
         assert.deepStrictEqual(
             ensure({}, { foo: { bar: map } }),
             { foo: { bar: map } }
-        );
-    });
-
-    it("should cast existing value to Map", () => {
-        assert.deepStrictEqual(
-            ensure({ foo: map }, { foo: Map }),
-            { foo: map }
-        );
-        assert.deepStrictEqual(
-            ensure({ foo: entries }, { foo: Map }),
-            { foo: map }
-        );
-    });
-
-    it("should cast existing value in sub-nodes to Map", () => {
-        assert.deepStrictEqual(
-            ensure({ foo: { bar: entries } }, { foo: { bar: Map } }),
-            { foo: { bar: map } }
-        );
-    });
-
-    it("should throw proper error if casting failed", () => {
-        let err;
-
-        try {
-            ensure({ foo: "not a Map" }, { foo: Map });
-        } catch (e) {
-            err = e;
-        }
-
-        assert.deepStrictEqual(
-            String(err),
-            "TypeError: The value of 'foo' is not a Map and cannot be casted into one"
-        );
-    });
-
-    it("should throw proper error if casting failed in sub-nodes", () => {
-        let err;
-
-        try {
-            ensure({ foo: { bar: [1, 2, 3] } }, { foo: { bar: Map } });
-        } catch (e) {
-            err = e;
-        }
-
-        assert.deepStrictEqual(
-            String(err),
-            "TypeError: The value of 'foo.bar' is not a Map and cannot be casted into one"
         );
     });
 });
