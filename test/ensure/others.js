7d374e8af7cf080edeca573993d37779b1df1c50
Ayon Lee <i@hyurl.com>
2020-04-23 12:44:05 +0800

    fix test

--- test/ensure/others.js
+++ test/ensure/others.js
@@ -3,12 +3,14 @@ const assert = require("assert");
 const { ensure } = require("../..");
 
 describe("ensure: Other Types", () => {
-    it("should ensure default value of functions", () => {
+    it("should ensure the value of functions", () => {
         let fn1 = () => { };
         let fn2 = function () { };
 
+        assert.deepStrictEqual(ensure({}, { foo: fn1 }), { foo: fn1 });
+        assert.deepStrictEqual(ensure({}, { foo: fn2 }), { foo: fn2 });
         assert.deepStrictEqual(
-            ensure({}, { foo: fn1, bar: fn2 }),
+            ensure({ foo: fn1, bar: fn2 }, { foo: fn1, bar: Function }),
             { foo: fn1, bar: fn2 }
         );
     });
@@ -16,7 +18,7 @@ describe("ensure: Other Types", () => {
     it("should ensure the value of user-defined types", () => {
         class A { }
 
-        const a = new A;
+        const a = new A();
 
         assert.deepStrictEqual(ensure({}, { foo: A }), { foo: null });
         assert.deepStrictEqual(ensure({}, { foo: a }), { foo: a });
